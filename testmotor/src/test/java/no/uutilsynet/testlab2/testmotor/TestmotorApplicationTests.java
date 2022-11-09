1e61159abe2e9f7942e947586a292fdd509896ec
oho <oystein.hoyum@digdir.no>
2022-11-09 14:16:32 +0100

    Init repo testlab2-core

---
+++ testmotor/src/test/java/no/uutilsynet/testlab2/testmotor/TestmotorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.uutilsynet.testlab2.testmotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmotorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
