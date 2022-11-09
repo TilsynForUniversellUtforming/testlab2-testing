73ccbfc8d5558d5dd36e8051fdc11abd391f9377
oho <oystein.hoyum@digdir.no>
2022-11-09 14:15:56 +0100

    Init repo testlab2-core

---
+++ src/main/java/no/uutilsynet/testlab2/Testlab2CoreApplication.java
@@ -0,0 +1,13 @@
+package no.uutilsynet.testlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testlab2CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testlab2CoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/uutilsynet/testlab2/Testlab2CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package no.uutilsynet.testlab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testlab2CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
