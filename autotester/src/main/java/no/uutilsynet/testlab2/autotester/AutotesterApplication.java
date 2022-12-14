e1290ebc1ee9fbc3d7eaf1d6666ff5b31687dfe0
oho <oystein.hoyum@digdir.no>
2022-12-14 15:14:19 +0100

    Testoppsett

---
+++ autotester/src/main/java/no/uutilsynet/testlab2/autotester/AutotesterApplication.java
@@ -0,0 +1,13 @@
+package no.uutilsynet.testlab2.autotester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutotesterApplication.class, args);
+    }
+
+}
---
+++ autotester/src/main/java/no/uutilsynet/testlab2/autotester/web/CrawlingResults.java
@@ -0,0 +1,6 @@
+package no.uutilsynet.testlab2.autotester.web;
+
+import java.util.List;
+
+public record CrawlingResults (List<String> nettsider) {
+}
