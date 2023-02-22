c65b3cf136fd5c99e105af5e7eaab38e8d94b77f
Lars Lønne <lonnelars@gmail.com>
2023-02-22 14:03:00 +0100

    omskriving
    Innfører en enum for statuser, i stedet for å bruke String.
    Samler sql for lagring av måling.
    Fjerner updateStatus, som egentlig ikke gjorde så mye lenger.

--- src/test/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingTest.kt
@@ -1,34 +1,19 @@
 package no.uutilsynet.testlab2testing.maaling
 
-import no.uutilsynet.testlab2testing.maaling.TestConstants.loeysingList
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MaalingTest {
-  @Test
-  @DisplayName("vi skal kunne oppdatere status fra planlegging til crawling")
-  fun updateStatus() {
-    val result =
-        Maaling.updateStatus(
-                Maaling.Planlegging(1, "testmåling", loeysingList), "crawling", listOf())
-            .getOrThrow()
-
-    Assertions.assertTrue(result is Maaling.Crawling)
-    assertThat(result.id, equalTo(1))
-    assertThat(result.navn, equalTo("testmåling"))
-  }
-
   @Nested
   @DisplayName("validering av status")
   inner class ValidateStatus {
     @Test
     @DisplayName("'crawling' er en gyldig ny status")
     fun crawlingIsOk() {
-      assertThat(validateStatus("crawling"), equalTo(Result.success("crawling")))
+      assertThat(validateStatus("crawling"), equalTo(Result.success(Status.Crawling)))
     }
   }
 }
