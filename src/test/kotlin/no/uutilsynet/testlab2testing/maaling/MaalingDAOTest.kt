f507b2a8faae031a60c33508757f27ada97c6f8d
Lars Lønne <lonnelars@gmail.com>
2023-03-14 15:22:26 +0100

    fikser lesing av crawlresultat fra databasen
    Fikser en bug som gjorde at vi hoppet over et resultat fra databasen, hvis resultatet før hadde flere nettsider.
    
    DFK-175

--- src/test/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingDAOTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingDAOTest.kt
@@ -2,6 +2,7 @@ package no.uutilsynet.testlab2testing.maaling
 
 import java.net.URL
 import java.time.Instant
+import no.uutilsynet.testlab2testing.maaling.TestConstants.loeysingList
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -23,17 +24,24 @@ class MaalingDAOTest(@Autowired val maalingDAO: MaalingDAO) {
     assertThat(maalingFromDatabase.crawlResultat).isNotEmpty
     maalingFromDatabase.crawlResultat.forEach {
       val nettsider = (it as CrawlResultat.Ferdig).nettsider
-      assertThat(nettsider).hasSize(1)
-      assertThat(nettsider.first()).isEqualTo(it.loeysing.url)
+      assertThat(nettsider)
+          .contains(
+              URL(it.loeysing.url, "/"),
+              URL(it.loeysing.url, "/underside/1"),
+              URL(it.loeysing.url, "/underside/2"))
     }
   }
 
   private fun saveMaalingWithStatusKvalitetssikring(): Int {
-    val id = maalingDAO.createMaaling("testmåling", listOf(1, 2))
+    val id = maalingDAO.createMaaling("testmåling", loeysingList.map { it.id })
     val maaling = maalingDAO.getMaaling(id) as Maaling.Planlegging
     val crawlResultat =
         maaling.loeysingList.map {
-          CrawlResultat.Ferdig(listOf(it.url), URL("https://status.uri"), it, Instant.now())
+          CrawlResultat.Ferdig(
+              listOf(URL(it.url, "/"), URL(it.url, "/underside/1"), URL(it.url, "/underside/2")),
+              URL("https://status.uri"),
+              it,
+              Instant.now())
         }
     val kvalitetssikring = Maaling.toKvalitetssikring(Maaling.toCrawling(maaling, crawlResultat))!!
     maalingDAO.save(kvalitetssikring).getOrThrow()
