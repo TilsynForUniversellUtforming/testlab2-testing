87c7739c220a696cbc04cddcffa913435c1fdf66
Lars Lønne <lonnelars@gmail.com>
2023-05-10 11:14:08 +0200

    fjerner klassen Features
    Vi har ingen aktive features nå, så denne klassen gjør ingenting.

--- src/main/kotlin/no/uutilsynet/testlab2testing/Features.kt
+++
@@ -1,5 +0,0 @@
-package no.uutilsynet.testlab2testing
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "features") data class Features(val startTesting: Boolean)
