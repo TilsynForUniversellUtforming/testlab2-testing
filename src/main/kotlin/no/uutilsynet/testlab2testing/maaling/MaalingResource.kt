1ddfb041732373406f2ad0576c1e2288bfee6a70
Lars Lønne <lonnelars@gmail.com>
2023-08-15 14:59:33 +0200

    omskriving
    Flytter koden som oppdaterer målinger til en egen klasse.
    
    DFK-307

--- src/main/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/maaling/MaalingResource.kt
@@ -9,7 +9,6 @@ import java.net.URL
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
-import java.util.concurrent.TimeUnit.SECONDS
 import kotlinx.coroutines.*
 import no.uutilsynet.testlab2testing.common.ErrorHandlingUtil.handleErrors
 import no.uutilsynet.testlab2testing.common.validateIdList
@@ -27,7 +26,6 @@ import no.uutilsynet.testlab2testing.toSingleResult
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -338,72 +336,6 @@ class MaalingResource(
       }
 
   data class StatusDTO(val status: String, val loeysingIdList: List<Int>?)
-
-  @Scheduled(fixedDelay = 30, timeUnit = SECONDS)
-  fun updateStatuses() {
-    val alleMaalinger: List<Maaling> =
-        maalingDAO.getMaalingListByStatus(listOf(MaalingStatus.crawling, MaalingStatus.testing))
-
-    runCatching {
-          val statusCrawling = alleMaalinger.filterIsInstance<Maaling.Crawling>()
-          val oppdaterteMaalinger =
-              statusCrawling.map { updateCrawlingStatuses(it) }.toSingleResult().getOrThrow()
-          oppdaterteMaalinger.map { maalingDAO.save(it) }.toSingleResult().getOrThrow()
-          if (statusCrawling.isNotEmpty()) {
-            logger.info(
-                "oppdaterte status for ${statusCrawling.size} målinger med status `crawling`")
-          }
-        }
-        .getOrElse {
-          logger.error("klarte ikke å oppdatere status for målinger med status `crawling`", it)
-        }
-
-    runCatching {
-          val statusTesting = alleMaalinger.filterIsInstance<Maaling.Testing>()
-          val oppdaterteMaalinger = statusTesting.map { updateTestingStatuses(it) }
-          oppdaterteMaalinger.map { maalingDAO.save(it) }.toSingleResult().getOrThrow()
-          if (statusTesting.isNotEmpty()) {
-            logger.info("oppdaterte status for ${statusTesting.size} målinger med status `testing`")
-          }
-        }
-        .getOrElse {
-          logger.error(
-              "klarte ikke å oppdatere status for målinger med status `testing` i maalingDAO", it)
-        }
-  }
-
-  private fun updateCrawlingStatuses(maaling: Maaling): Result<Maaling> =
-      when (maaling) {
-        is Maaling.Crawling -> {
-          runCatching {
-            val oppdaterteResultater =
-                maaling.crawlResultat
-                    .filterIsInstance<CrawlResultat.IkkeFerdig>()
-                    .map {
-                      crawlerClient.getStatus(it).map { newStatus -> updateStatus(it, newStatus) }
-                    }
-                    .toSingleResult()
-                    .getOrThrow()
-            val oppdatertMaaling = maaling.copy(crawlResultat = oppdaterteResultater)
-            Maaling.toKvalitetssikring(oppdatertMaaling) ?: oppdatertMaaling
-          }
-        }
-        else -> Result.success(maaling)
-      }
-
-  private fun updateTestingStatuses(maaling: Maaling.Testing): Maaling {
-    val oppdaterteTestKoeyringar =
-        maaling.testKoeyringar.map { testKoeyring ->
-          autoTesterClient.updateStatus(testKoeyring).getOrElse {
-            logger.error(
-                "feila da eg forsøkte å hente test status for løysing id ${testKoeyring.loeysing.id} på måling id ${maaling.id}",
-                it)
-            testKoeyring
-          }
-        }
-    val oppdatertMaaling = maaling.copy(testKoeyringar = oppdaterteTestKoeyringar)
-    return Maaling.toTestingFerdig(oppdatertMaaling) ?: oppdatertMaaling
-  }
 
   private suspend fun startTesting(
       maalingId: Int,
