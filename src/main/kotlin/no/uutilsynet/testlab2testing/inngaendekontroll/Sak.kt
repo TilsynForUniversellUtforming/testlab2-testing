4abb09bba3138d1521b6a9cfca8f048b805ec4b6
Lars Lønne <lonnelars@gmail.com>
2023-11-23 14:47:34 +0100

    oppdaterer sak med loeysinger
    Legger til oppdatering av en sak, hvor vi kan legge til eller fjerne løsninger.
    
    DFK-375

--- src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/Sak.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/Sak.kt
@@ -1,3 +1,5 @@
 package no.uutilsynet.testlab2testing.inngaendekontroll
 
-data class Sak(val virksomhet: String)
+data class Sak(val virksomhet: String, val loeysingar: List<Loeysing> = emptyList()) {
+  data class Loeysing(val loeysingId: Int)
+}
--- src/test/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/SakResourceTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/SakResourceTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForObject
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,7 +19,8 @@ class SakResourceTest(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun opprettingAvEiSak() {
       val virksomhet = "123456785"
-      val result = restTemplate.postForEntity("/saker", virksomhet, Unit::class.java)
+      val result =
+          restTemplate.postForEntity("/saker", mapOf("virksomhet" to virksomhet), Unit::class.java)
       assertThat(result.statusCode).isEqualTo(HttpStatus.CREATED)
       assertThat(result.headers.location).isNotNull()
     }
@@ -27,7 +29,8 @@ class SakResourceTest(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun ugyldigOrgnummer() {
       val virksomhet = "123456789"
-      val result = restTemplate.postForEntity("/saker", virksomhet, Unit::class.java)
+      val result =
+          restTemplate.postForEntity("/saker", mapOf("virksomhet" to virksomhet), Unit::class.java)
       assertThat(result.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
 
@@ -35,9 +38,46 @@ class SakResourceTest(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun hentSak() {
       val virksomhet = "123456785"
-      val result = restTemplate.postForEntity("/saker", virksomhet, Unit::class.java)
+      val result =
+          restTemplate.postForEntity("/saker", mapOf("virksomhet" to virksomhet), Unit::class.java)
       val sak: Sak = restTemplate.getForObject(result.headers.location!!, Sak::class.java)!!
       assertThat(sak.virksomhet).isEqualTo(virksomhet)
+    }
+  }
+
+  @DisplayName("oppdatering av ei sak")
+  @Nested
+  inner class OppdateringAvEiSak {
+    @DisplayName("vi skal kunne legge til løysingar på ei sak")
+    @Test
+    fun leggTilEiLoeysing() {
+      val location = restTemplate.postForLocation("/saker", mapOf("virksomhet" to "123456785"))!!
+      val sak: Sak = restTemplate.getForObject(location)!!
+
+      val sakMedLoeysingar: Sak = sak.copy(loeysingar = listOf(Sak.Loeysing(1), Sak.Loeysing(2)))
+      restTemplate.put(location, sakMedLoeysingar)
+
+      val sakEtterOppdatering: Sak = restTemplate.getForObject(location)!!
+      assertThat(sakEtterOppdatering.loeysingar).hasSize(2)
+    }
+
+    @DisplayName("vi skal kunne fjerne løysingar på ei sak")
+    @Test
+    fun fjerneLoeysingar() {
+      val location = restTemplate.postForLocation("/saker", mapOf("virksomhet" to "123456785"))!!
+      val sak: Sak = restTemplate.getForObject(location)!!
+
+      val sakMedLoeysingar: Sak = sak.copy(loeysingar = listOf(Sak.Loeysing(1), Sak.Loeysing(2)))
+      restTemplate.put(location, sakMedLoeysingar)
+
+      val sakEtterOppdatering: Sak = restTemplate.getForObject(location)!!
+      assertThat(sakEtterOppdatering.loeysingar).hasSize(2)
+
+      val sakUtanLoeysingar: Sak = sak.copy(loeysingar = emptyList())
+      restTemplate.put(location, sakUtanLoeysingar)
+
+      val sakEtterFjerning: Sak = restTemplate.getForObject(location)!!
+      assertThat(sakEtterFjerning.loeysingar).isEmpty()
     }
   }
 }
