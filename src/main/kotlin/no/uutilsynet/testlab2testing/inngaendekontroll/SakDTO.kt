bad8fcc709882b306501c8832d5e8649c3859086
oho <oystein.hoyum@digdir.no>
2024-01-11 13:31:02 +0100

    DFK-432 DFK-405 Rebase

--- src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/SakDTO.kt
+++
@@ -1,9 +0,0 @@
-package no.uutilsynet.testlab2testing.inngaendekontroll
-
-import no.uutilsynet.testlab2testing.testregel.TestregelDTO
-
-data class SakDTO(
-    val virksomhet: String,
-    val loeysingar: List<Sak.Loeysing> = emptyList(),
-    val testreglar: List<TestregelDTO> = emptyList()
-) {}
