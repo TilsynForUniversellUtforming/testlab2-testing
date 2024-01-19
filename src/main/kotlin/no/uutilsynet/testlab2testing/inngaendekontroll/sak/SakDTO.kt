b97914588cd92a3aa6f0db5c204f454c46f3f76d
oho <oystein.hoyum@digdir.no>
2024-01-19 14:44:33 +0100

    DFK-405 Justert output. Rebase

--- src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/sak/SakDTO.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/sak/SakDTO.kt
@@ -1,5 +1,6 @@
 package no.uutilsynet.testlab2testing.inngaendekontroll.sak
 
+import java.time.LocalDate
 import no.uutilsynet.testlab2testing.brukar.Brukar
 import no.uutilsynet.testlab2testing.testregel.TestregelDTO
 
@@ -7,6 +8,7 @@ data class SakDTO(
     val id: Int,
     val namn: String,
     val virksomhet: String,
+    val frist: LocalDate,
     val ansvarleg: Brukar?,
     val loeysingar: List<Sak.Loeysing> = emptyList(),
     val testreglar: List<TestregelDTO> = emptyList()
