31d2fdff85b5c0c4026083165644d129b1585edf
Lars Lønne <lars.lonne@kantega.no>
2024-01-23 12:52:55 +0100

    forslag til status på testresultat
    DFK-438

--- src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/testresultat/ResultatManuellKontroll.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/testresultat/ResultatManuellKontroll.kt
@@ -1,5 +1,7 @@
 package no.uutilsynet.testlab2testing.inngaendekontroll.testresultat
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.Instant
 import no.uutilsynet.testlab2testing.brukar.Brukar
 
@@ -14,7 +16,29 @@ data class ResultatManuellKontroll(
     val elementResultat: String?,
     val elementUtfall: String?,
     val svar: List<Svar>,
-    val testVartUtfoert: Instant?
+    val testVartUtfoert: Instant?,
+    val status: Status? = null
 ) {
   data class Svar(val steg: String, val svar: String)
+
+  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+  @JsonSubTypes(
+      JsonSubTypes.Type(value = Status.IkkePaabegynt::class, name = "ikke_paabegynt"),
+      JsonSubTypes.Type(value = Status.Paabegynt::class, name = "paabegynt"),
+      JsonSubTypes.Type(value = Status.Ferdig::class, name = "ferdig"),
+      JsonSubTypes.Type(value = Status.Deaktivert::class, name = "deaktivert"))
+  sealed class Status {
+    object IkkePaabegynt : Status()
+
+    object Paabegynt : Status()
+
+    data class Ferdig(
+        val elementOmtale: String,
+        val elementResultat: String,
+        val elementUtfall: String,
+        val testVartUtfoert: Instant
+    ) : Status()
+
+    object Deaktivert : Status()
+  }
 }
