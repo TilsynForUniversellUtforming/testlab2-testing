93702be5be7c90911f1a32cc686d03652c4e324d
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2024-03-04 12:49:18 +0100

    DFK-498 - revert fjerning av TestregelModus "forenklet"

--- src/main/kotlin/no/uutilsynet/testlab2testing/testregel/TestregelModus.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/testregel/TestregelModus.kt
@@ -3,5 +3,6 @@ package no.uutilsynet.testlab2testing.testregel
 enum class TestregelModus(val value: String) {
   automatisk("automatisk"),
   semiAutomatisk("semi-automatisk"),
-  manuell("manuell")
+  manuell("manuell"),
+  forenklet("forenklet")
 }
