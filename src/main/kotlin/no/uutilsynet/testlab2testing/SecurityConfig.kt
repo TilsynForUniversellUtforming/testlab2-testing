840305a6b247b47eddd48bb814cb42e4504f2189
oho <oystein.hoyum@digdir.no>
2024-03-08 13:15:26 +0100

    DFK-252 Feature toggle på sikkerhet

--- src/main/kotlin/no/uutilsynet/testlab2testing/SecurityConfig.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/SecurityConfig.kt
@@ -3,9 +3,11 @@ package no.uutilsynet.testlab2testing
 import no.uutilsynet.testlab2securitylib.Testlab2AuthenticationConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
@@ -15,6 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 @EnableWebSecurity
 class SecurityConfig {
   @Bean
+  @Profile("security")
   open fun filterChain(http: HttpSecurity): SecurityFilterChain {
 
     http {
@@ -22,9 +25,20 @@ class SecurityConfig {
       oauth2ResourceServer {
         jwt { jwtAuthenticationConverter = Testlab2AuthenticationConverter() }
       }
-      csrf { disable() }
+      sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
+      csrf { disable() } //
     }
 
+    return http.build()
+  }
+
+  @Bean
+  @Profile("!security")
+  fun openFilterChain(http: HttpSecurity): SecurityFilterChain {
+    http {
+      authorizeHttpRequests { authorize(anyRequest, permitAll) }
+      cors {}
+    }
     return http.build()
   }
 
