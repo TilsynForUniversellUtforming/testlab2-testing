72638d8ef4620d4e5c42fe69a2dda10eaf7d036e
Lars Lønne <lonnelars@gmail.com>
2024-03-14 13:13:52 +0100

    feat: endepunkt for å hente ut en kontroll
    DFK-511

--- src/test/kotlin/no/uutilsynet/testlab2testing/kontroll/KontrollResourceTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/kontroll/KontrollResourceTest.kt
@@ -1,6 +1,10 @@
 package no.uutilsynet.testlab2testing.kontroll
 
+import io.restassured.RestAssured.get
 import io.restassured.RestAssured.given
+import io.restassured.path.json.JsonPath
+import io.restassured.path.json.JsonPath.from
+import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.startsWith
 import org.junit.jupiter.api.DisplayName
@@ -52,5 +56,34 @@ class KontrollResourceTest {
             .extract()
             .header("Location")
     given().port(port).delete(location).then().statusCode(equalTo(204))
+  }
+
+  @Test
+  @DisplayName(
+      "gitt at vi har opprettet en kontroll, så skal vi kunne hente den ut igjen med url-en i location")
+  fun getKontrollById() {
+    val body =
+        mapOf(
+            "tittel" to "testkontroll",
+            "saksbehandler" to "Ola Nordmann",
+            "sakstype" to "Forvaltningssak",
+            "arkivreferanse" to "1234")
+    val location =
+        given()
+            .port(port)
+            .body(body)
+            .contentType("application/json")
+            .post("/kontroller")
+            .then()
+            .statusCode(equalTo(201))
+            .extract()
+            .header("Location")
+    val responseBody = get(location).asString()
+    val json: JsonPath = from(responseBody)
+
+    assertThat(json.get<String>("tittel")).isEqualTo("testkontroll")
+    assertThat(json.get<String>("saksbehandler")).isEqualTo("Ola Nordmann")
+    assertThat(json.get<String>("sakstype")).isEqualTo("Forvaltningssak")
+    assertThat(json.get<String>("arkivreferanse")).isEqualTo("1234")
   }
 }
