76c8411c7104cc2d5d26f84b5037538e20034d9c
oho <oystein.hoyum@digdir.no>
2024-05-10 14:36:17 +0200

    DFK-549 Opprydding etter rebase

--- src/main/kotlin/no/uutilsynet/testlab2testing/loeysing/LoeysingsRegisterClient.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/loeysing/LoeysingsRegisterClient.kt
@@ -33,7 +33,7 @@ class LoeysingsRegisterClient(
             "loeysingsregisteret returnerte ikkje ein location da vi oppretta ei ny løysing")
   }
 
-  @Cacheable("loeysingar", unless = "#result==null")
+  @Cacheable("loeysingar", unless = "#result == Result.Failure")
   fun getMany(idList: List<Int>): Result<List<Loeysing>> = getMany(idList, Instant.now())
 
   fun getMany(idList: List<Int>, tidspunkt: Instant): Result<List<Loeysing>> {
