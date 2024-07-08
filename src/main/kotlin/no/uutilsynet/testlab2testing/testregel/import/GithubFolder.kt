f366e377402c45afecf27bd412475ee03ff14442
oho <oystein.hoyum@digdir.no>
2024-07-08 10:46:13 +0200

    DFK-602 Uthenting av data frå github

---
+++ src/main/kotlin/no/uutilsynet/testlab2testing/testregel/import/GithubFolder.kt
@@ -0,0 +1,14 @@
+data class GithubFolder(
+    val name: String,
+    val path: String,
+    val url: String,
+    val html_url: String,
+    val git_url: String,
+    val download_url: String?,
+    val type: String,
+    val content: String?,
+    val encoding: String?
+
+)
+
+
