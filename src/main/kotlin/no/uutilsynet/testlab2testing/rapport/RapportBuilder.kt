b77ba026e7a74fbccdad212272e6093b5153b733
Øystein Høyum <oystein.hoyum@digdir.no>
2024-08-20 10:05:14 +0200

    Dfk 606 generere rapport (#132)
    * DFK-606 oppretta dato på kontroll. Api endepunkt for å kalle rapport-app

---
+++ src/main/kotlin/no/uutilsynet/testlab2testing/rapport/RapportBuilder.kt
@@ -0,0 +1,100 @@
+package no.uutilsynet.testlab2testing.rapport
+
+import java.net.URI
+import java.time.LocalDate
+import java.time.ZoneId
+import no.uutilsynet.testlab2testing.inngaendekontroll.testresultat.ResultatManuellKontroll
+import no.uutilsynet.testlab2testing.kontroll.KontrollDAO
+import no.uutilsynet.testlab2testing.kontroll.Sideutval
+import no.uutilsynet.testlab2testing.krav.KravregisterClient
+import no.uutilsynet.testlab2testing.loeysing.Loeysing
+import no.uutilsynet.testlab2testing.testregel.Testregel
+import no.uutilsynet.testlab2testing.testregel.TestregelDAO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class RapportBuilder(
+    @Autowired val testregelDAO: TestregelDAO,
+    @Autowired val kontrollDAO: KontrollDAO,
+    @Autowired val kravregisterClient: KravregisterClient
+) {
+
+  var rapportNummer: String? = null
+  var datoFra: LocalDate = LocalDate.now()
+  var datoTil: LocalDate = LocalDate.now()
+  var verksemd: String? = null
+  var loeysing: Loeysing.Expanded? = null
+  var avvik: List<Avvik>? = null
+  var kontrollId: Int? = null
+  var sider: List<Sideutval> = emptyList()
+
+  fun kontroll(kontroll: KontrollDAO.KontrollDB) = apply {
+    rapportNummer = kontroll.arkivreferanse
+    datoFra = LocalDate.ofInstant(kontroll.opprettaDato, ZoneId.of("Europe/Oslo"))
+    kontrollId = kontroll.id
+  }
+
+  fun loeysing(loeysing: Loeysing.Expanded) = apply {
+    this.loeysing = loeysing
+    this.verksemd = loeysing.verksemd?.namn
+    this.sider = siderForKontoll(loeysing.id)
+  }
+
+  fun siderForKontoll(loysingId: Int): List<Sideutval> =
+      kontrollId?.let { kontrollDAO.findSideutvalByKontrollAndLoeysing(it, listOf(loysingId)) }
+          ?: throw IllegalStateException("KontrollId is not set")
+
+  fun testresultat(testresultat: List<ResultatManuellKontroll>) = apply {
+    avvik = testresultat.groupBy { it.testregelId }.values.map(::toIndexedAvvik).flatten()
+  }
+
+  private fun toIndexedAvvik(testresultat: List<ResultatManuellKontroll>): List<Avvik> {
+    return testresultat.mapIndexed { index, resultat -> resultat.toAvvik(index) }.subList(0, 1)
+  }
+
+  fun build(): Rapport {
+    return Rapport(
+        rapportNummer = rapportNummer ?: "",
+        datoTil = datoFra.toString(),
+        datoFra = datoTil.toString(),
+        verksemd = verksemd ?: "",
+        loeysing = loeysing?.namn ?: "",
+        avvik = avvik ?: emptyList())
+  }
+
+  fun ResultatManuellKontroll.toAvvik(index: Int): Avvik {
+
+    val testregel =
+        testregelDAO.getTestregel(this.testregelId)
+            ?: throw IllegalStateException("Testregel not found for id ${this.testregelId}")
+
+    val side = finnSide(this.sideutvalId)
+    return Avvik(
+        nummer = index + 1,
+        resultatId = this.id,
+        testregel = testregel.toTestregelRapport(),
+        side = Side(side.first, side.second.begrunnelse, side.second.url),
+        elementOmtale = "",
+        elementResultat = this.elementResultat.toString(),
+        elementUtfall = this.elementUtfall,
+        tema =
+            testregel.let {
+              testregelDAO.getTemaForTestregel().first { tema -> tema.id == it.tema }.tema
+            })
+  }
+
+  fun Testregel.toTestregelRapport(): TestregelRapport {
+    val krav = kravregisterClient.getWcagKrav(this.kravId).getOrThrow()
+    return TestregelRapport(
+        testregelId = this.id,
+        testregelNoekkel = this.testregelId,
+        kravId = this.kravId,
+        kravTittel = krav.tittel,
+        kravUrl = krav.urlRettleiing?.let { URI.create(it) })
+  }
+
+  fun finnSide(sideutvalId: Int): Pair<Int, Sideutval> {
+    return sider.filter { it.id == sideutvalId }.map { Pair(sider.indexOf(it), it) }.first()
+  }
+}
---
+++ src/main/kotlin/no/uutilsynet/testlab2testing/rapport/RapportService.kt
@@ -0,0 +1,74 @@
+package no.uutilsynet.testlab2testing.rapport
+
+import java.util.*
+import no.uutilsynet.testlab2testing.forenkletkontroll.logger
+import no.uutilsynet.testlab2testing.inngaendekontroll.testgrunnlag.kontroll.TestgrunnlagKontrollDAO
+import no.uutilsynet.testlab2testing.inngaendekontroll.testresultat.TestResultatDAO
+import no.uutilsynet.testlab2testing.kontroll.KontrollDAO
+import no.uutilsynet.testlab2testing.loeysing.LoeysingsRegisterClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.http.*
+import org.springframework.http.converter.ByteArrayHttpMessageConverter
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestTemplate
+
+@Service
+class RapportService(
+    @Autowired val restTemplate: RestTemplate,
+    @Autowired val rapportBuilder: RapportBuilder,
+    @Autowired val testResultatDAO: TestResultatDAO,
+    @Autowired val testgrunnlagKontrollDAO: TestgrunnlagKontrollDAO,
+    @Autowired val kontrollDAO: KontrollDAO,
+    @Autowired val loeysingsRegisterClient: LoeysingsRegisterClient,
+    @Autowired val properties: RapportVerktoeyKlient
+) {
+  fun opprettRapport(kontrollId: Int, loeysingId: Int): Rapport {
+
+    val kontroll = kontrollDAO.getKontroller(listOf(kontrollId)).getOrThrow().first()
+    val loeysing = loeysingsRegisterClient.getManyExpanded(listOf(loeysingId)).getOrThrow().first()
+
+    val testgrunnlag = testgrunnlagKontrollDAO.getOpprinneligTestgrunnlag(kontroll.id).getOrThrow()
+    val testresultat = testResultatDAO.getManyResults(testgrunnlag).getOrThrow()
+
+    return rapportBuilder.kontroll(kontroll).loeysing(loeysing).testresultat(testresultat).build()
+  }
+
+  fun createWordRapport(rapport: Rapport): ByteArray {
+    restTemplate.messageConverters.add(0, ByteArrayHttpMessageConverter())
+
+    val restClient = RestClient.builder(restTemplate).build()
+
+    val requestBody =
+        mapOf(
+            "rapportNummer" to rapport.rapportNummer,
+            "datoFra" to rapport.datoFra,
+            "datoTil" to rapport.datoTil,
+            "verksemd" to rapport.verksemd,
+            "loeysing" to rapport.loeysing,
+            "avvik" to rapport.avvik)
+
+    val response =
+        restClient
+            .post()
+            .uri(properties.host + "/rapport/lagRapport")
+            .body(requestBody)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(
+                MediaType.valueOf(
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+            .retrieve()
+            .onStatus(HttpStatusCode::isError) { _, response ->
+              logger.error(response.body.readAllBytes().contentToString())
+            }
+            .body(ByteArray::class.java)
+
+    return response ?: throw IllegalStateException("No response body")
+  }
+}
+
+@ConfigurationProperties(prefix = "rapportverktoey")
+data class RapportVerktoeyKlient(val host: String)
+
+data class Testresponse(val message: String, val number: Int)
