fd9ad17370310791702856ba66e59a3b7e284b73
oho <oystein.hoyum@digdir.no>
2024-11-11 13:49:08 +0100

    DFk-664

--- src/test/kotlin/no/uutilsynet/testlab2testing/ITTestSecurityConfig.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/ITTestSecurityConfig.kt
@@ -1,12 +1,18 @@
 package no.uutilsynet.testlab2testing
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.client.RestTemplate
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +20,7 @@ import org.springframework.security.web.SecurityFilterChain
 class ITTestSecurityConfig {
 
   @Bean
-  open fun testFilterChain(http: HttpSecurity): SecurityFilterChain {
+  fun testFilterChain(http: HttpSecurity): SecurityFilterChain {
 
     http {
       authorizeHttpRequests { authorize(anyRequest, permitAll) }
@@ -22,5 +28,19 @@ class ITTestSecurityConfig {
     }
 
     return http.build()
+  }
+
+  @Bean
+  fun restTemplate(
+      restTemplateBuilder: RestTemplateBuilder,
+  ): RestTemplate {
+    val objectMapper =
+        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    val mappingJackson2HttpMessageConverter = MappingJackson2HttpMessageConverter()
+    mappingJackson2HttpMessageConverter.objectMapper = objectMapper
+    mappingJackson2HttpMessageConverter.supportedMediaTypes =
+        listOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM)
+
+    return restTemplateBuilder.messageConverters(mappingJackson2HttpMessageConverter).build()
   }
 }
