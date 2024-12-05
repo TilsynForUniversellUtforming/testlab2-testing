4409f46c4a3891995fb486123bfef45516f60248
oho <oystein.hoyum@digdir.no>
2024-12-05 16:17:24 +0100

    DFk-668 Testopprydding

--- src/main/kotlin/no/uutilsynet/testlab2testing/forenkletkontroll/MaalingResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/forenkletkontroll/MaalingResource.kt
@@ -60,7 +60,10 @@ class MaalingResource(
                 val location = locationForId(id)
                 ResponseEntity.created(location).build()
               },
-              { exception -> ErrorHandlingUtil.handleErrors(exception) })
+              { exception ->
+                logger.error(exception.message)
+                ErrorHandlingUtil.handleErrors(exception)
+              })
 
   @PutMapping
   fun updateMaaling(@RequestBody dto: EditMaalingDTO): ResponseEntity<out Any> =
