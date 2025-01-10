155d917a3cfb6c2ba872d2ceaf6d6c9d5156cb25
oho <oystein.hoyum@digdir.no>
2025-01-10 15:44:41 +0100

    DFK-682 Fjerna null-typar

--- src/main/kotlin/no/uutilsynet/testlab2testing/resultat/ResultatService.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/resultat/ResultatService.kt
@@ -1,5 +1,6 @@
 package no.uutilsynet.testlab2testing.resultat
 
+import java.time.LocalDate
 import no.uutilsynet.testlab2.constants.Kontrolltype
 import no.uutilsynet.testlab2testing.dto.TestresultatDetaljert
 import no.uutilsynet.testlab2testing.ekstern.resultat.EksternResultatDAO
@@ -13,7 +14,6 @@ import no.uutilsynet.testlab2testing.loeysing.LoeysingsRegisterClient
 import no.uutilsynet.testlab2testing.testregel.TestregelService
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
-import java.time.LocalDate
 
 @Component
 class ResultatService(
@@ -374,7 +374,7 @@ class ResultatService(
                   result.map { it.testar }.flatten().distinct(),
                   result.map { it.score }.average(),
                   kravId,
-              result.first().kravTittel,
+                  result.first().kravTittel,
                   talTestaElement(result),
                   result.sumOf { it.talElementBrot },
                   result.sumOf { it.talElementSamsvar })
