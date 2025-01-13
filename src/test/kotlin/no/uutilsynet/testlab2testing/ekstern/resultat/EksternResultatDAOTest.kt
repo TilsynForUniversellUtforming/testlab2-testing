fbf767135ad4a5fe98170686d7bbf4b77349184d
oho <oystein.hoyum@digdir.no>
2025-01-13 13:11:25 +0100

    DFK-683 Fiks feil boolsk sjekk på publiseringsstatus

--- src/test/kotlin/no/uutilsynet/testlab2testing/ekstern/resultat/EksternResultatDAOTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/ekstern/resultat/EksternResultatDAOTest.kt
@@ -12,10 +12,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest(properties = ["spring.datasource.url= jdbc:tc:postgresql:16-alpine:///test-db"])
 @ActiveProfiles("test")
 class EksternResultatDAOTest(
     @Autowired val eksternResultatDAO: EksternResultatDAO,
