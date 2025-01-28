2d53ff8ccb46ca0ffddf39a7aa441178f36c9929
oho <oystein.hoyum@digdir.no>
2025-01-28 14:12:03 +0100

    DFK-690

--- src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/dokumentasjon/BildeResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/inngaendekontroll/dokumentasjon/BildeResource.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.Scheduled
@@ -62,10 +63,15 @@ class BildeResource(val bildeService: BildeService) {
   fun getBilde(@PathVariable("bildesti") bildesti: String): ResponseEntity<InputStreamResource> {
 
     val bildeConnection = bildeService.getBilde(bildesti)
-    val contentType = MediaType.parseMediaType(bildeConnection.contentType)
+    val contentType: String = bildeConnection.contentType ?: "image/jpeg"
+    val mediaType = MediaType.parseMediaType(contentType)
+
+    val headers = HttpHeaders()
+    headers["Content-Disposition"] = "inline; filename=\"$bildesti\""
 
     return ResponseEntity.ok()
-        .contentType(contentType)
+        .headers(headers)
+        .contentType(mediaType)
         .body(InputStreamResource(bildeService.getBilde(bildesti).inputStream))
   }
 }
