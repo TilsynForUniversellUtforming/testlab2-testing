c1cbc7b5b89cee966694af603839b83b730c662a
oho <oystein.hoyum@digdir.no>
2025-02-17 09:13:32 +0100

    DFK-700 Logging testar

--- src/main/resources/db/migration/V95__fk_aggs.sql
+++ src/main/resources/db/migration/V95__fk_aggs.sql
@@ -1,14 +1,14 @@
 ALTER TABLE aggregering_testregel
-ADD CONSTRAINT aggregering_testregel_testgrunnlag_fk(testgrunnlag_id)
+ADD CONSTRAINT aggregering_testregel_testgrunnlag_fk foreign key (testgrunnlag_id)
 REFERENCES Testgrunnlag(id)
 ON DELETE CASCADE;
 
 ALTER TABLE aggregering_side
-ADD CONSTRAINT aggregering_side_testgrunnlag_fk(testgrunnlag_id)
+ADD CONSTRAINT aggregering_side_testgrunnlag_fk foreign key(testgrunnlag_id)
 REFERENCES Testgrunnlag(id)
 ON DELETE CASCADE;
 
 ALTER TABLE aggregering_suksesskriterium
-ADD CONSTRAINT aggregering_suksesskriterium_testgrunnlag_fk(testgrunnlag_id)
+ADD CONSTRAINT aggregering_suksesskriterium_testgrunnlag_fk foreign key(testgrunnlag_id)
 REFERENCES Testgrunnlag(id)
 ON DELETE CASCADE;
