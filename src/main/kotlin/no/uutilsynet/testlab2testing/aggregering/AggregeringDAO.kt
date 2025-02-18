26f482fc49413e7269c9b022e1b8086b0aa27cdc
oho <oystein.hoyum@digdir.no>
2025-02-18 12:51:39 +0100

    DFK-693

--- src/main/kotlin/no/uutilsynet/testlab2testing/aggregering/AggregeringDAO.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/aggregering/AggregeringDAO.kt
@@ -1,12 +1,12 @@
 package no.uutilsynet.testlab2testing.aggregering
 
+import java.net.URI
+import java.sql.ResultSet
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.jdbc.support.KeyHolder
 import org.springframework.stereotype.Component
-import java.net.URI
-import java.sql.ResultSet
 
 @Component
 class AggregeringDAO(val jdbcTemplate: NamedParameterJdbcTemplate) {
