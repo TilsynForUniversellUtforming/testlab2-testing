fa2b83f13edfb5a24dfba99e1b27bed113a55c2e
oho <oystein.hoyum@digdir.no>
2025-06-20 10:15:08 +0200

    DFK-783 Div refaktorering

--- src/main/kotlin/no/uutilsynet/testlab2testing/ekstern/resultat/TestresultatDetaljertEkstern.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/ekstern/resultat/TestresultatDetaljertEkstern.kt
@@ -1,12 +1,12 @@
 package no.uutilsynet.testlab2testing.ekstern.resultat
 
+import java.net.URL
+import java.time.LocalDateTime
 import no.uutilsynet.testlab2.constants.TestresultatUtfall
 import no.uutilsynet.testlab2testing.dto.TestresultatDetaljert
 import no.uutilsynet.testlab2testing.inngaendekontroll.testresultat.Bilde
 import no.uutilsynet.testlab2testing.testregel.Testregel
 import org.springframework.web.util.UriComponentsBuilder
-import java.net.URL
-import java.time.LocalDateTime
 
 data class TestresultatDetaljertEkstern(
     val testregelNoekkel: String,
