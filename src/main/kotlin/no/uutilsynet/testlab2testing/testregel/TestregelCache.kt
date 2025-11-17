493b55fe2b256298e860013da77a041c00087cf7
hoyum <oystein.hoyum@digdir.no>
2025-11-17 09:11:28 +0100

    DFK-848 skru av tracing

--- src/main/kotlin/no/uutilsynet/testlab2testing/testregel/TestregelCache.kt
+++ src/main/kotlin/no/uutilsynet/testlab2testing/testregel/TestregelCache.kt
@@ -3,26 +3,32 @@ package no.uutilsynet.testlab2testing.testregel
 import org.springframework.stereotype.Service
 
 @Service
-class TestregelCache(testregelService: TestregelService) {
+class TestregelCache(private val testregelService: TestregelService) {
   private val cacheKey: MutableMap<String, TestregelKrav> = mutableMapOf()
   private val cacheIds: MutableMap<Int, TestregelKrav> = mutableMapOf()
 
-  init {
-    testregelService.getTestregelKravList().forEach { testregel ->
-      cacheKey[testregel.testregelId] = testregel
-      cacheIds[testregel.id] = testregel
-    }
-  }
-
   /*@Observed(name = "testregelcache.getBykey")*/
   fun getTestregelByKey(testregelKey: String): TestregelKrav {
+      if(cacheIds.isEmpty()){
+          init()
+      }
     return cacheKey[testregelKey]
         ?: throw NoSuchElementException("Testregel not found: $testregelKey")
   }
 
   /*@Observed(name = "testregelcache.getbyid")*/
   fun getTestregelById(testregelId: Int): TestregelKrav {
+      if(cacheIds.isEmpty()){
+          init()
+      }
     return cacheIds[testregelId]
         ?: throw NoSuchElementException("Testregel not found for id: $testregelId")
   }
+
+    fun init() {
+        testregelService.getTestregelKravList().forEach { testregel ->
+            cacheKey[testregel.testregelId] = testregel
+            cacheIds[testregel.id] = testregel
+        }
+    }
 }
