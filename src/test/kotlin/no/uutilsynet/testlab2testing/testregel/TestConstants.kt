35499d271e49ee096e2c9136df940140605a9ba7
hoyum <oystein.hoyum@digdir.no>
2026-01-28 10:32:11 +0100

    DFK-859 Flytting av testreglar til krav

--- src/test/kotlin/no/uutilsynet/testlab2testing/testregel/TestConstants.kt
+++ src/test/kotlin/no/uutilsynet/testlab2testing/testregel/TestConstants.kt
@@ -1,30 +1,30 @@
-//package no.uutilsynet.testlab2testing.testregel
-//
-//import no.uutilsynet.testlab2.constants.TestregelInnholdstype
-//import no.uutilsynet.testlab2.constants.TestregelModus
-//import no.uutilsynet.testlab2.constants.TestregelStatus
-//import no.uutilsynet.testlab2testing.common.TestlabLocale
-//
-//object TestConstants {
-//  val name = "test_skal_slettes"
-//  val testregelTestKravId = 1
-//  val testregelSchemaAutomatisk = "QW-ACT-R70"
-//  val testregelSchemaManuell = """{ "gaaTil": 1 }"""
-//  val modus = TestregelModus.automatisk
-//
-//  val testregelCreateRequestBody =
-//      mapOf(
-//          "testregelId" to testregelSchemaAutomatisk,
-//          "namn" to name,
-//          "kravId" to testregelTestKravId,
-//          "status" to TestregelStatus.publisert,
-//          "type" to TestregelInnholdstype.nett,
-//          "modus" to modus,
-//          "spraak" to TestlabLocale.nb,
-//          "testregelSchema" to testregelSchemaAutomatisk,
-//          "innhaldstypeTesting" to 1,
-//          "tema" to 1,
-//          "testobjekt" to 1,
-//          "kravTilSamsvar" to "",
-//      )
-//}
+package no.uutilsynet.testlab2testing.testregel
+
+import no.uutilsynet.testlab2.constants.TestregelInnholdstype
+import no.uutilsynet.testlab2.constants.TestregelModus
+import no.uutilsynet.testlab2.constants.TestregelStatus
+import no.uutilsynet.testlab2testing.common.TestlabLocale
+
+object TestConstants {
+  val name = "test_skal_slettes"
+  val testregelTestKravId = 1
+  val testregelSchemaAutomatisk = "QW-ACT-R70"
+  val testregelSchemaManuell = """{ "gaaTil": 1 }"""
+  val modus = TestregelModus.automatisk
+
+  val testregelCreateRequestBody =
+      mapOf(
+          "testregelId" to testregelSchemaAutomatisk,
+          "namn" to name,
+          "kravId" to testregelTestKravId,
+          "status" to TestregelStatus.publisert,
+          "type" to TestregelInnholdstype.nett,
+          "modus" to modus,
+          "spraak" to TestlabLocale.nb,
+          "testregelSchema" to testregelSchemaAutomatisk,
+          "innhaldstypeTesting" to 1,
+          "tema" to 1,
+          "testobjekt" to 1,
+          "kravTilSamsvar" to "",
+      )
+}
